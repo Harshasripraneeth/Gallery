d26205da254b8ae723edfe24942e8f8fe1fdfd81
Harshasripraneeth <46346478+Harshasripraneeth@users.noreply.github.com>
2021-04-20 22:26:19 +0530

    added delete api endpoint, helps in deleting the request

---
+++ api/delete.php
@@ -0,0 +1,38 @@
+<?php
+
+//headers
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+header('Content-Type: application/json');
+
+if($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+
+    if(isset($_POST['location'])){
+
+    //database connection      
+    include_once $_SERVER['DOCUMENT_ROOT'] ."/api/config/Database.php";
+    $database = new Database();
+    $db = $database->getMYSQLI();
+
+    //getting file location
+    $location = $_POST['location'];
+    $path = substr('./'.$_POST['location'],23);
+    unlink($path);
+    $query = "DELETE  FROM images where location = '$location' ";
+
+    if( $db->query($query)){
+    echo json_encode(array('message'=> 'success'));
+    }
+    else{
+    echo json_encode(array('message'=> 'error occurred'));
+    }
+
+    
+ }
+}
+else{
+	echo json_encode(array("message"=>"please ensure the request method is POST"));
+} 
+
+?>
